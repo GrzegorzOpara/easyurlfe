6ead4b557574dc569aec3ed57c5ca54b3ee029da
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-03 10:59:33 +0000

    added email and password validation

--- src/pages/CreateUserPage.js
+++ src/pages/CreateUserPage.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { Form, FormField, Text, Box, TextInput, Button, Tip } from "grommet";
+import validator from 'validator';
 
 const CreateUserPage = () => {
 
+    const [email, setEmail] = useState(null)
+    const [validEmail, setValidEmail] = useState(true)
+    const [emailMatch, setEmailMatch] = useState(true)
+    
+    const [password, setPassword] = useState(null)
+    const [validPassword, setValidPassword] = useState(true)
+    const [passwordsMatch, setPasswordsMatch] = useState(true)
+    
+    
+    
     const handleSubmit = (e) => {
         // Handle submit form function
     }
@@ -11,11 +23,27 @@ const CreateUserPage = () => {
     }
 
     const validatePassword = (e) => {
-        // validate if password is fine (complexity, match 1 & 2)
+        if (validator.isStrongPassword(e) === true)
+            setValidPassword(true)
+        else
+        setValidPassword(false)
+    }
+
+    const handlePrimaryPasswordChange = (e) => {
+        setPassword(e.target.value)
+        validatePassword(e.target.value)
+    }
+
+    const handleSecondaryPasswordChange = (e) => {
+        if (e.target.value === password)
+            setPasswordsMatch(true)
+        else
+            setPasswordsMatch(false)
     }
 
     const validateEmail = (e) => {
-        // validate if password is fine (complexity, match 1 & 2)
+        setValidEmail(validator.isEmail(e))
+        console.log(validator.isEmail(e))
     }
 
     return (
@@ -23,27 +51,30 @@ const CreateUserPage = () => {
             <Form onSubmit={(event) => handleSubmit(event)}>
                 <Box>
                     <FormField>
-                        <TextInput name="username"></TextInput>
+                        <TextInput name="username" ></TextInput>
                     </FormField>
                 </Box>
                 <Box>
                     <FormField>
-                        <TextInput name="password"></TextInput>
+                        <TextInput name="passwordPrimary" onChange={(e) => handlePrimaryPasswordChange(e)}></TextInput>
+                        {validPassword? null : <Box>Passwords doesn't meet complexity requirements</Box>}
                     </FormField>
                 </Box>
                 <Box>
                     <FormField>
-                        <TextInput name="password2"></TextInput>
+                        <TextInput name="passwordSecondary"  onChange={(e) => handleSecondaryPasswordChange(e)}></TextInput>
+                        {passwordsMatch? null : <Box>Passwords doesn't match</Box>}
                     </FormField>
                 </Box>
                 <Box>
                     <FormField>
-                        <TextInput name="email1"></TextInput>
+                        <TextInput name="emailPrimary" placeholder="email address" onBlur={(e) => validateEmail(e.target.value)} ></TextInput>
+                        {validEmail? null : <Box>The email address is not valid</Box>}
                     </FormField>
                 </Box>
                 <Box>
                     <FormField>
-                        <TextInput name="email2"></TextInput>
+                        <TextInput name="emailSecondary"></TextInput>
                     </FormField>
                 </Box>
                 <Box pad="small"><Button type="submit" primary={true}>Create User</Button></Box>
