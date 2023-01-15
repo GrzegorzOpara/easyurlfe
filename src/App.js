d12294795cbdf829ca587a5f96aa2a357327d971
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-15 09:58:15 +0100

    password reset page init

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreateUserPage from './pages/CreateUserPage';
 import WelcomePage from './pages/WelcomePage';
 import LoginPage from './pages/LoginPage'
+import PasswordResetPage from './pages/PasswordResetPage';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/welcome" element={<WelcomePage />}/>
             <Route path="/create" element={<CreateUserPage />}/>
             <Route path="/login" element={<LoginPage />}/>
+            <Route path="/password-reset" element={<PasswordResetPage />}/>
           </Routes>
         </AuthProvider>
       </Router>   
